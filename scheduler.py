224b83d306557b99422cdd84848856f62e848a1f
Varun Unnithan <varun.unnithan33@gmail.com>
2024-04-28 17:19:54 -0400

    finished cost function

--- scheduler.py
+++ scheduler.py
@@ -133,6 +133,47 @@ def airfoil_cost(input):
         return float('inf')
     
     # get the cl/cd
+    with open('./postProcessing/forces/0/forceCoeffs.dat', 'r') as f:
+
+        consider_csv = False
+        coeff_csv = ''
+        
+        for line in f:
+            if 'Cl' in line and 'Cd' in line:
+                consider_csv = True
+
+            if consider_csv:
+                coeff_csv += line + '\n'
+
+
+    # now we need to parse the csv
+    reader = csv.reader(coeff_csv.split('\n'), delimiter='\t')    
+    print(reader)
+    cd = 2
+    cl = 3
+    # look at first row to see where Cd and Cl are
+    for idx, row in enumerate(next(reader)):
+        row = row.strip() 
+        if row == 'Cd':
+            cd = idx
+        if row == 'Cl':
+            cl = idx
+    # go to last row to get the values
+    for row in reader:
+        if row:
+
+            # in case tab delimiters didn't parse correctly
+            if len(row) < 4:
+                row = row[0].split(' ')
+                row = [x for x in row if x]
+
+            # store latest cl cd
+            cd_val = row[cd]
+            cl_val = row[cl]
+
+    print(cd_val, cl_val)
+
+    return float(cl_val) / float(cd_val)
 
 
 
